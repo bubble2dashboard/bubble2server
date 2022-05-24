984813d86314279f99288bff46d0da731067f56e
Giovany Lopes <giovanylopes@Giovanys-MacBook-Pro.local>
2022-05-24 10:26:03 +0100

    basic user model added

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const conString = `mongodb+srv://bubble2dashboard:bubble2@cluster0.alj3z.mongodb.net/?retryWrites=true&w=majority`;
+
+const connectDB = async () => {
+	const db = await mongoose.connect(conString, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
+
+	console.log('mongodb connected');
+};
+
+module.exports = connectDB;
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const connectDB = require('./config/db.js');
+
+const User = require('./models/User.js');
+
+// connecting to database
+
+connectDB();
+
+app.listen(5000, function () {
+	console.log('server is listening at port 5000');
+});
+
+app.get('/', function (request, response) {
+	response.send('welcome to the server');
+});
+
+app.post('/api/auth/register', async function (req, res) {
+	const user = await User.create(req.body);
+	res.status(201).json({
+		success: true,
+		data: user,
+	});
+});
+
+app.post('/api/auth/login', async function (req, res) {
+	console.log(req.body.password);
+
+	const user = await User.find({
+		userName: req.body.userName,
+		password: req.body.password,
+	});
+
+	if (user) {
+		res.status(200).json({
+			success: true,
+			data: user,
+		});
+	} else {
+		res.status(400).json({
+			success: false,
+			data: null,
+		});
+	}
+});
